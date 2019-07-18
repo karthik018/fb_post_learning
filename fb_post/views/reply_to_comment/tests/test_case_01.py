04f40f5ecab01aaa780dafc8b272b2e7a44ff414
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-18 13:32:45 +0530

    updated tests using snapshots for reply to comment

--- fb_post/views/reply_to_comment/tests/test_case_01.py
+++ fb_post/views/reply_to_comment/tests/test_case_01.py
@@ -1,7 +1,8 @@
 """
 # TODO: Update test case description
 """
-from django_swagger_utils.drf_server.utils.server_gen.custom_api_test_case import CustomAPITestCase
+from django_swagger_utils.utils.test import CustomAPITestCase
+
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
 from fb_post.models.models import *
 
@@ -12,12 +13,6 @@ REQUEST_BODY = """
 }
 """
 
-RESPONSE_BODY = """
-{
-    "replyid": 1
-}
-"""
-
 TEST_CASE = {
     "request": {
         "path_params": {"postid": "ibgroup", "commentid": "ibgroup"},
@@ -25,19 +20,16 @@ TEST_CASE = {
         "header_params": {},
         "securities": {"oauth": {"tokenUrl": "http://auth.ibtspl.com/oauth2/", "flow": "password", "scopes": ["superuser"], "type": "oauth2"}},
         "body": REQUEST_BODY,
-    },
-    "response": {
-        "status": 201,
-        "body": RESPONSE_BODY,
-        "header_params": {}
     }
 }
 
 
 class TestCase01ReplyToCommentAPITestCase(CustomAPITestCase):
-
-    def __init__(self, *args, **kwargs):
-        super(TestCase01ReplyToCommentAPITestCase, self).__init__(APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX, TEST_CASE, *args, **kwargs)
+    app_name = APP_NAME
+    operation_name = OPERATION_NAME
+    request_method = REQUEST_METHOD
+    url_suffix = URL_SUFFIX
+    test_case_dict = TEST_CASE
 
     def setupUser(self, username, password):
         pass
@@ -53,20 +45,4 @@ class TestCase01ReplyToCommentAPITestCase(CustomAPITestCase):
         TEST_CASE['request']['path_params']['postid'] = self.post.id
         TEST_CASE['request']['path_params']['commentid'] = self.comment.id
         self.count_before = Comment.objects.filter(commented_on_id=self.comment.id).count()
-        super(TestCase01ReplyToCommentAPITestCase, self).test_case()
-
-    def compareResponse(self, response, test_case_response_dict):
-        count_after = Comment.objects.filter(commented_on_id=self.comment.id).count()
-        import json
-        response_date = json.loads(response.content)
-        reply_id = response_date['replyid']
-        reply = Comment.objects.get(id=reply_id)
-
-        assert count_after == self.count_before + 1
-        assert reply_id == 2
-        assert reply.commented_on_id.id == self.comment.id
-        assert reply.user.id == self.foo_user.id
-        assert reply.post.id == self.post.id
-
-
-
+        self.default_test_case()
