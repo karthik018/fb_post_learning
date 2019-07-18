a09ed08ea453150fca99d6d9b9f5773f121acdeb
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-18 13:13:56 +0530

    added tests for posts reacted by user

--- fb_post/views/posts_reacted_by_user/tests/test_case_01.py
+++ fb_post/views/posts_reacted_by_user/tests/test_case_01.py
@@ -1,12 +1,79 @@
 """
 # TODO: Update test case description
 """
-
-from django_swagger_utils.utils.test import CustomAPITestCase
+from django_swagger_utils.drf_server.utils.server_gen.custom_api_test_case import CustomAPITestCase
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
+from fb_post.models.models import *
 
 REQUEST_BODY = """
 
+"""
+
+RESPONSE_BODY = """
+{
+    "posts": [
+        {
+            "postid": 1,
+            "posted_by": {
+                "userid": 1,
+                "username": "string",
+                "profile_pic": "string"
+            },
+            "post_content": "string",
+            "post_create_date": "2099-12-31 00:00:00",
+            "reactions": {
+                "count": 1,
+                "types": [
+                    {
+                        "reaction": "LIKE"
+                    }
+                ]
+            },
+            "comment_count": 1,
+            "comments": [
+                {
+                    "comment_id": 1,
+                    "commenter": {
+                        "userid": 1,
+                        "username": "string",
+                        "profile_pic": "string"
+                    },
+                    "comment_message": "string",
+                    "comment_create_date": "2099-12-31 00:00:00",
+                    "reactions": {
+                        "count": 1,
+                        "types": [
+                            {
+                                "reaction": "LIKE"
+                            }
+                        ]
+                    },
+                    "replies_count": 1,
+                    "replies": [
+                        {
+                            "comment_id": 1,
+                            "commenter": {
+                                "userid": 1,
+                                "username": "string",
+                                "profile_pic": "string"
+                            },
+                            "comment_message": "string",
+                            "comment_create_date": "2099-12-31 00:00:00",
+                            "reactions": {
+                                "count": 1,
+                                "types": [
+                                    {
+                                        "reaction": "LIKE"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
 """
 
 TEST_CASE = {
@@ -17,17 +84,51 @@ TEST_CASE = {
         "securities": {},
         "body": REQUEST_BODY,
     },
+    "response": {
+        "status": 200,
+        "body": RESPONSE_BODY,
+        "header_params": {}
+    }
 }
 
 
 class TestCase01PostsReactedByUserAPITestCase(CustomAPITestCase):
-    app_name = APP_NAME
-    operation_name = OPERATION_NAME
-    request_method = REQUEST_METHOD
-    url_suffix = URL_SUFFIX
-    test_case_dict = TEST_CASE
+
+    def __init__(self, *args, **kwargs):
+        super(TestCase01PostsReactedByUserAPITestCase, self).__init__(APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX, TEST_CASE, *args, **kwargs)
+
+    def setupUser(self, username, password):
+        pass
+
+    def setup_data(self):
+        self.foo_user = self._create_user("user1", "password")
+        self.bar_user = self._create_user("user2", "password")
+        self.foo_bar_user = self._create_user("user3", "password")
+        self.post1 = Post.objects.create(user_id=self.foo_user.id, post_description="first post")
+        self.post2 = Post.objects.create(user_id=self.bar_user.id, post_description="second post")
+        self.post3 = Post.objects.create(user_id=self.foo_bar_user.id, post_description="third post")
+        self.react1_post1 = PostReaction.objects.create(post_id=self.post1.id, user_id=self.foo_user.id, reaction="LIKE")
+        self.react1_post2 = PostReaction.objects.create(post_id=self.post2.id, user_id=self.foo_user.id, reaction="LOVE")
+        self.react3_post1 = PostReaction.objects.create(post_id=self.post1.id, user_id=self.foo_bar_user.id, reaction="SAD")
+        self.react2_post1 = PostReaction.objects.create(post_id=self.post1.id, user_id=self.bar_user.id, reaction="HAHA")
+        self.react2_post2 = PostReaction.objects.create(post_id=self.post2.id, user_id=self.bar_user.id, reaction="WOW")
+        self.react3_post2 = PostReaction.objects.create(post_id=self.post2.id, user_id=self.foo_bar_user.id, reaction="WOW")
+        self.react1_post3 = PostReaction.objects.create(post_id=self.post3.id, user_id=self.foo_bar_user.id, reaction="SAD")
+        self.react2_post3 = PostReaction.objects.create(post_id=self.post3.id, user_id=self.foo_user.id, reaction="ANGRY")
+        self.react3_post3 = PostReaction.objects.create(post_id=self.post3.id, user_id=self.bar_user.id, reaction="WOW")
 
     def test_case(self):
-        self.default_test_case() # Returns response object.
-        # Which can be used for further response object checks.
-        # Add database state checks here.
+        self.setup_data()
+        TEST_CASE['request']['path_params']['username'] = self.foo_user.id
+        super(TestCase01PostsReactedByUserAPITestCase, self).test_case()
+
+    def compareResponse(self, response, test_case_response_dict):
+        import json
+        response_data = json.loads(response.content)
+        user_reacted_posts = response_data['posts']
+        post_ids = [user_reacted_posts[i]['postid'] for i in range(len(user_reacted_posts))]
+        assert len(user_reacted_posts) == 3
+        assert self.post1.id in post_ids
+        assert self.post2.id in post_ids
+        assert self.post3.id in post_ids
+        assert response.status_code == 200
