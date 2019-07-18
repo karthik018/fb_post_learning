3f0f41a51e2174725375802d915b620eb4b6e822
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-18 13:28:18 +0530

    updated tests using snapshots for create post

--- fb_post/views/create_post/tests/test_case_01.py
+++ fb_post/views/create_post/tests/test_case_01.py
@@ -1,23 +1,14 @@
 """
 # TODO: Update test case description
 """
-from django_swagger_utils.drf_server.utils.server_gen.custom_api_test_case import CustomAPITestCase
 
+from django_swagger_utils.utils.test import CustomAPITestCase
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
-import json
-from fb_post.models.models import *
-
 
 REQUEST_BODY = """
 {
     "post_content": "Test_Post_content",
     "post_create_date": "2099-12-31 00:00:00"
-}
-"""
-
-RESPONSE_BODY = """
-{
-    "postid": 1
 }
 """
 
@@ -28,33 +19,16 @@ TEST_CASE = {
         "header_params": {},
         "securities": {"oauth": {"tokenUrl": "http://auth.ibtspl.com/oauth2/", "flow": "password", "scopes": ["superuser"], "type": "oauth2"}},
         "body": REQUEST_BODY,
-    },
-    "response": {
-        "status": 201,
-        "body": RESPONSE_BODY,
-        "header_params": {}
     }
 }
 
 
 class TestCase01CreatePostAPITestCase(CustomAPITestCase):
-
-    def __init__(self, *args, **kwargs):
-        super(TestCase01CreatePostAPITestCase, self).__init__(APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX,
-                                                              TEST_CASE, *args, **kwargs)
+    app_name = APP_NAME
+    operation_name = OPERATION_NAME
+    request_method = REQUEST_METHOD
+    url_suffix = URL_SUFFIX
+    test_case_dict = TEST_CASE
 
     def test_case(self):
-        self.count_before = Post.objects.count()
-        print(self.count_before)
-        super(TestCase01CreatePostAPITestCase, self).test_case()
-
-    def compareResponse(self, response, test_case_response_dict):
-        super(TestCase01CreatePostAPITestCase, self).compareResponse(response, test_case_response_dict)
-        count_after = Post.objects.count()
-
-        response_data = json.loads(response.content)
-        post = Post.objects.get(id=response_data["postid"])
-
-        assert post.post_description == "Test_Post_content"
-        assert count_after == self.count_before + 1
-        assert post.user == self.foo_user
+        self.default_test_case()
