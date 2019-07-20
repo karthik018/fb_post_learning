10feb5e8452dbbba6528416a911607755879e898
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-20 14:34:45 +0530

    added create_post interactor

---
+++ fb_post_v2/interactors/create_post_interactor.py
@@ -0,0 +1,13 @@
+from fb_post_v2.interactors.storages.post_storage import PostStorage
+from fb_post_v2.interactors.presenters.json_presenter import JsonPresenter
+
+class CreatePostInteractor:
+
+    def __init__(self, post_storage: PostStorage, presenter: JsonPresenter):
+        self.post_storage = post_storage
+        self.presenter = presenter
+
+    def create_post(self, post_content: str, created_by: int) -> dict:
+        post_dto = self.post_storage.create_post(post_content, created_by)
+        response = self.presenter.create_post(post_dto)
+        return response
