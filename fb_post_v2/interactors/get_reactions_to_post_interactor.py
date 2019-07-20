056cb2d70dc7bf8a11d4f3de007273e1c5535c8d
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-20 14:37:47 +0530

    added get_reactions_to_post interactor

---
+++ fb_post_v2/interactors/get_reactions_to_post_interactor.py
@@ -0,0 +1,14 @@
+from fb_post_v2.interactors.presenters.json_presenter import JsonPresenter
+from fb_post_v2.interactors.storages.post_storage import PostStorage
+
+
+class GetReactionsToPostInteractor:
+
+    def __init__(self, post_storage: PostStorage, presenter: JsonPresenter):
+        self.post_storage = post_storage
+        self.presenter = presenter
+
+    def get_reactions_to_post(self, post_id: int) -> dict:
+        reactions_dto = self.post_storage.get_reactions_to_post(post_id)
+        response = self.presenter.get_reactions_to_post(reactions_dto)
+        return response
