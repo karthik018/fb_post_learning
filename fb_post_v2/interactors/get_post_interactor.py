1c0cfb11bc3bc6657c341cd44b21a7e78671dadf
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-20 14:35:21 +0530

    added get_post interactor

---
+++ fb_post_v2/interactors/get_post_interactor.py
@@ -0,0 +1,14 @@
+from fb_post_v2.interactors.presenters.json_presenter import JsonPresenter
+from fb_post_v2.interactors.storages.post_storage import PostStorage
+
+
+class GetPostInteractor:
+
+    def __init__(self, post_storage: PostStorage, presenter: JsonPresenter):
+        self.post_storage = post_storage
+        self.presenter = presenter
+
+    def get_post(self, post_id: int) -> dict:
+        get_post_dto = self.post_storage.get_post(post_id=post_id)
+        response = self.presenter.get_post(get_post_dto)
+        return response
