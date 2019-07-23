943f8580028b4ed85408a848d78b1ff4dea60d38
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-23 09:48:26 +0530

    added tests for create_comment storage

--- fb_post_v2/tests/storages/test_create_comment_storage.py
+++ fb_post_v2/tests/storages/test_create_comment_storage.py
@@ -22,3 +22,24 @@ class TestCreateComment:
 
         assert comment_id_dto.comment_id == 1
         assert comment.message == comment_content
+
+@pytest.mark.django_db
+class TestCreateReply:
+
+    @freeze_time("2012-03-26")
+    @pytest.fixture
+    def setup_data(self):
+        self.user = User.objects.create(username='karthik', profile_pic='http://karthik.png')
+        self.post = Post.objects.create(user_id=self.user.id, post_description="first post")
+        self.first_comment = Comment.objects.create(post_id=self.post.id, user_id=self.user.id, message="first comment")
+
+    def test_create_reply(self, setup_data):
+        post_storage = PostStorage()
+        comment_content = "first reply"
+
+        comment_id_dto = post_storage.create_reply(self.first_comment.id, self.user.id, comment_content)
+        comment = Comment.objects.get(id=comment_id_dto.comment_id)
+
+        assert comment_id_dto.comment_id == 2
+        assert comment.commented_on_id == self.first_comment.id
+
