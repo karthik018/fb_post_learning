8a5b752c3ed3fe6057f1850179943b267c9c83d7
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-23 15:06:39 +0530

    added tests for react to comment presenter

--- fb_post_v2/tests/presenters/test_reaction_presenter.py
+++ fb_post_v2/tests/presenters/test_reaction_presenter.py
@@ -12,3 +12,14 @@ class TestReactToPost:
 
         assert response["reaction_id"] == reaction_dto.reaction_id
 
+
+class TestReactToComment:
+
+    def test_react_to_comment(self):
+        presenter = JsonPresenter()
+
+        reaction_dto = ReactionIdDTO(reaction_id=2)
+
+        response = presenter.react_to_comment(reaction_dto)
+
+        assert response["reaction_id"] == reaction_dto.reaction_id
