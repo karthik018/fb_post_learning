62e63a5efcc9bbe7a8e399fcfabd9333bca134e7
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-23 13:23:26 +0530

    added tests for post not exists and delete post presenter

---
+++ fb_post_v2/tests/presenters/test_post_not_exists_presenter.py
@@ -0,0 +1,27 @@
+from fb_post_v2.interactors.storages.post_storage import PostIdDTO
+from fb_post_v2.presenters.presenter import JsonPresenter
+import pytest
+
+
+class TestPostNotExists:
+
+    def test_post_not_exists(self):
+        presenter = JsonPresenter()
+
+        from django_swagger_utils.drf_server.exceptions import BadRequest
+        with pytest.raises(BadRequest):
+            response = presenter.post_not_exists()
+
+class TestDeletePost:
+
+    def test_delete_post(self):
+        presenter = JsonPresenter()
+
+        post_id_dto = PostIdDTO(post_id=None)
+
+        response = presenter.delete_post(post_id_dto)
+
+        assert response["post_id"] == post_id_dto.post_id
+
+
+
