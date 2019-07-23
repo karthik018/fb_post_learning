8bc5b77c576b6fe4864b9d6e79eeab6bf649932a
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-23 16:27:43 +0530

    added api wrapper to add comment to post

--- fb_post_v2/views/comment_to_post/api_wrapper.py
+++ fb_post_v2/views/comment_to_post/api_wrapper.py
@@ -1,29 +1,24 @@
 from django_swagger_utils.drf_server.utils.decorator.interface_decorator \
     import validate_decorator
-from .validator_class import ValidatorClass
 
+from fb_post_v2.interactors.create_comment_interactor import CreateCommentInteractor
+from .validator_class import ValidatorClass
+from fb_post_v2.storages.post_storage import PostStorage
+from fb_post_v2.presenters.presenter import JsonPresenter
 
 @validate_decorator(validator_class=ValidatorClass)
 def api_wrapper(*args, **kwargs):
-    # ---------MOCK IMPLEMENTATION---------
+    user = kwargs['user']
+    post_id = kwargs['postid']
 
-    try:
-        from fb_post_v2.views.comment_to_post.tests.test_case_01 \
-            import TEST_CASE as test_case
-    except ImportError:
-        from fb_post_v2.views.comment_to_post.tests.test_case_01 \
-            import test_case
+    request_data = kwargs['request_data']
 
-    from django_swagger_utils.drf_server.utils.server_gen.mock_response \
-        import mock_response
-    try:
-        from fb_post_v2.views.comment_to_post.request_response_mocks \
-            import RESPONSE_200_JSON
-    except ImportError:
-        RESPONSE_200_JSON = ''
-    response_tuple = mock_response(
-        app_name="fb_post_v2", test_case=test_case,
-        operation_name="comment_to_post",
-        kwargs=kwargs, default_response_body=RESPONSE_200_JSON,
-        group_name="")
-    return response_tuple[1]
+    post_storage = PostStorage()
+    presenter = JsonPresenter()
+
+    interactor = CreateCommentInteractor(post_storage, presenter)
+
+    response = interactor.create_comment(post_id, user.id, request_data['comment_message'])
+
+    import json
+    return json.dumps(response)
