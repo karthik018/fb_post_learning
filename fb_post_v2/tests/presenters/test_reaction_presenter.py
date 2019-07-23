5bf5c629f8520fb1ca254a0ef6ffe0847bd27be2
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-23 15:02:53 +0530

    added tests for react to post respone presenter

---
+++ fb_post_v2/tests/presenters/test_reaction_presenter.py
@@ -0,0 +1,14 @@
+from fb_post_v2.interactors.storages.post_storage import ReactionIdDTO
+from fb_post_v2.presenters.presenter import JsonPresenter
+
+class TestReactToPost:
+
+    def test_react_to_post_response(self):
+        presenter = JsonPresenter()
+
+        reaction_dto = ReactionIdDTO(reaction_id=1)
+
+        response = presenter.react_to_post_response(reaction_dto)
+
+        assert response["reaction_id"] == reaction_dto.reaction_id
+
