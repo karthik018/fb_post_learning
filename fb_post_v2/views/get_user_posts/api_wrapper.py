40ce76018cc572b2e8ff4a82cf322048ae37967c
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-23 16:47:13 +0530

    added api wrapper to get user posts

--- fb_post_v2/views/get_user_posts/api_wrapper.py
+++ fb_post_v2/views/get_user_posts/api_wrapper.py
@@ -1,29 +1,25 @@
 from django_swagger_utils.drf_server.utils.decorator.interface_decorator \
     import validate_decorator
+
+from fb_post_v2.interactors.get_user_posts_interactor import GetUserPostsInteractor
 from .validator_class import ValidatorClass
+from fb_post_v2.storages.post_storage import PostStorage
+from fb_post_v2.presenters.presenter import JsonPresenter
 
 
 @validate_decorator(validator_class=ValidatorClass)
 def api_wrapper(*args, **kwargs):
-    # ---------MOCK IMPLEMENTATION---------
+    user = kwargs['username']
+    query_params = kwargs['request_query_params']
+    offset = query_params.offset
+    limit = query_params.limit
 
-    try:
-        from fb_post_v2.views.get_user_posts.tests.test_case_01 \
-            import TEST_CASE as test_case
-    except ImportError:
-        from fb_post_v2.views.get_user_posts.tests.test_case_01 \
-            import test_case
+    post_storage = PostStorage()
+    presenter = JsonPresenter()
 
-    from django_swagger_utils.drf_server.utils.server_gen.mock_response \
-        import mock_response
-    try:
-        from fb_post_v2.views.get_user_posts.request_response_mocks \
-            import RESPONSE_200_JSON
-    except ImportError:
-        RESPONSE_200_JSON = ''
-    response_tuple = mock_response(
-        app_name="fb_post_v2", test_case=test_case,
-        operation_name="get_user_posts",
-        kwargs=kwargs, default_response_body=RESPONSE_200_JSON,
-        group_name="")
-    return response_tuple[1]
+    interactor = GetUserPostsInteractor(post_storage, presenter)
+
+    response = interactor.get_user_posts(user, offset, limit)
+
+    import json
+    return json.dumps(response)
