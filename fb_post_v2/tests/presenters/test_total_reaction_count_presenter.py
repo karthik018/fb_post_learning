9ff4f23516aa34686f05d1d04b492ce3b8acb140
karthik018 <thinkcreative01karthik@gmail.com>
2019-07-23 13:33:25 +0530

    added tests for reaction metrics presenter

--- fb_post_v2/tests/presenters/test_total_reaction_count_presenter.py
+++ fb_post_v2/tests/presenters/test_total_reaction_count_presenter.py
@@ -1,4 +1,4 @@
-from fb_post_v2.interactors.storages.post_storage import TotalReactionDTO
+from fb_post_v2.interactors.storages.post_storage import TotalReactionDTO, ReactionCountDTO
 from fb_post_v2.presenters.presenter import JsonPresenter
 
 class TestTotalReactionCount:
@@ -12,4 +12,24 @@ class TestTotalReactionCount:
 
         assert response["total_count"] == count_dto.count
 
-class
+class TestReactionMetrics:
+
+    def test_reaction_metrics(self):
+        presenter = JsonPresenter()
+
+        count1_dto = ReactionCountDTO(count=2, reaction="LOVE")
+        count2_dto = ReactionCountDTO(count=3, reaction="LIKE")
+
+        reactions_dto = [count1_dto, count2_dto]
+
+        response = presenter.get_reaction_metrics(reactions_dto)
+
+        assert len(response["reactions"]) == len(reactions_dto)
+
+        test_reaction = {}
+        for reaction in response["reactions"]:
+            if reaction["reaction"] == count1_dto.reaction:
+                test_reaction = reaction
+
+        assert test_reaction["count"] == count1_dto.count
+        assert test_reaction["reaction"] == count1_dto.reaction
